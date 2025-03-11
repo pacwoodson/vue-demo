f59e7efbd97eff463b67571650a7b3d43633c9b7
pacoccino <pac@opac.me>
2025-03-11 15:23:33 +0700

    layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -8,33 +8,37 @@ const uiStore = useUIStore()
 </script>
 
 <template>
-  <header class="flex align-center bg-blue-100 dark:bg-gray-900 py-4 gap-8 px-4">
-    <img alt="Vue logo" class="w-8" src="@/assets/logo.svg" width="125" height="125" />
-    <span class="text-2xl">Vue Demo</span>
+  <div class="min-h-screen flex flex-col">
+    <header class="flex align-center bg-blue-100 dark:bg-gray-900 py-4 gap-8 px-4">
+      <img alt="Vue logo" class="w-8" src="@/assets/logo.svg" width="125" height="125" />
+      <span class="text-2xl">Vue Demo</span>
 
-    <nav class="flex flex-1 gap-4 align-center">
-      <RouterLink
-        v-for="route in router.options.routes.filter((route) => !route.meta?.hide)"
-        :key="route.path"
-        :to="route.path"
-        active-class="text-red-800 dark:text-red-400"
-      >
-        {{ route.name }}
-      </RouterLink>
-    </nav>
+      <nav class="flex flex-1 gap-4 align-center">
+        <RouterLink
+          v-for="route in router.options.routes.filter((route) => !route.meta?.hide)"
+          :key="route.path"
+          :to="route.path"
+          active-class="text-red-800 dark:text-red-400"
+        >
+          {{ route.name }}
+        </RouterLink>
+      </nav>
 
-    <div class="flex gap-4 align-center">
-      <button @click="uiStore.toggleDarkMode">
-        {{ uiStore.isDarkMode ? 'Dark' : 'Light' }}
-      </button>
-    </div>
-  </header>
+      <div class="flex gap-4 align-center">
+        <button @click="uiStore.toggleDarkMode">
+          {{ uiStore.isDarkMode ? 'Dark' : 'Light' }}
+        </button>
+      </div>
+    </header>
 
-  <main class="flex items-center container mx-auto w-screen-lg max-w-screen-lg w-full pb-8">
-    <RouterView />
-  </main>
+    <main
+      class="flex flex-1 items-center container mx-auto w-screen-lg max-w-screen-lg w-full pb-8"
+    >
+      <RouterView />
+    </main>
 
-  <footer class="flex align-center justify-center bg-blue-50 py-4 gap-4">
-    <a href="https://github.com/pacwoodson/vue-demo" target="_blank">Github</a>
-  </footer>
+    <footer class="flex align-center justify-center bg-blue-100 dark:bg-gray-900 py-4">
+      <a href="https://github.com/pacwoodson/vue-demo" target="_blank">Github</a>
+    </footer>
+  </div>
 </template>
