123abd5fc77a9945ba93492d85da4b503eecb145
pacoccino <pac@opac.me>
2025-03-13 11:48:36 +0700

    lol

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,21 +2,35 @@
 
 <template>
   <div class="prose">
-    <h2 class="my-8">Links to documentation</h2>
-    <div class="flex flex-col gap-4">
-      <a href="https://vuejs.org/">Vue</a>
-
-      <h3>Outils</h3>
-      <a href="https://router.vuejs.org/">Router</a>
-      <a href="https://pinia.vuejs.org/">Pinia</a>
-
-      <h3>Frameworks</h3>
-      <a href="https://nuxt.com/">Nuxt</a>
-
-      <h3>UI</h3>
-      <a href="https://tailwindcss.com/">TailwindCSS</a>
-      <a href="https://daisyui.com/">DaisyUI</a>
-      <a href="https://www.shadcn-vue.com/">Shadcn Vue</a>
-    </div>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
+    <h1 class="text-uppercase">Je n'ai pas compris</h1>
+    <h1 class="text-uppercase">L'exercice</h1>
   </div>
 </template>
